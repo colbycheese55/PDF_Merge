0a3f6d15bf226a58f61e33d03e635010e711336c
colbycheese55 <112818133+colbycheese55@users.noreply.github.com>
2024-01-11 15:40:19 -0500

    switched to PyMuPDF

--- src/backend.py
+++ src/backend.py
@@ -1,36 +1,35 @@
-import PyPDF2 as pdf
+import fitz
 import re as regex
 import os
 import datetime
 
 
-def executeInstruction(char: str, fileMap: dict[str, pdf.PdfReader], mergePDF: pdf.PdfMerger, start=1, end=None) -> str | bool: #TODO rework error messages
+def executeInstruction(char: str, fileMap: dict[str, "PDF"], mergePDF: "PDF", start=1, end=None) -> str | bool:
     if char not in fileMap:
         return f"\"{char}\" was not assigned to a file"
-    length = len(fileMap[char].pages)
+    length = len(fileMap[char])
     if start <= 0 or (end is not None and end <=0) or (end is not None and start > end):
         return f"Invalid page(s)"
     if start > length or (end is not None and end > length):
         return f"Out of range, this PDF has {length} pages"
     if end is None:
         end = length
-    mergePDF.append(fileMap[char], None, (start-1, end))
+    mergePDF.appendPages(fileMap[char], start, end)
     return True
 
-def saveMergedPDF(savepath: str, mergePDF: pdf.PdfMerger) -> None:
+def saveMergedPDF(savepath: str, mergePDF: "PDF") -> None:
     if savepath is not None and not savepath.endswith(".pdf"):
         savepath += ".pdf"
     if savepath is None:
         desktop_path = os.path.expanduser("~") + "\\Desktop"
         timestamp = datetime.datetime.now().strftime("%S%M%H")
         savepath = f"{desktop_path}\\merged_{timestamp}.pdf"
-    with open(savepath, "wb") as out:
-        mergePDF.write(out)
+    mergePDF.writeFile(savepath)
 
-def processInstructions(instructions: str, fileMap: dict[str, pdf.PdfReader], savepath: str) -> str:
+def processInstructions(instructions: str, fileMap: dict[str, "PDF"], savepath: str) -> str:
     instructions = instructions.lower().replace(" ", "").replace("\n", "")
     instructions = instructions.split(",")
-    mergePDF = pdf.PdfMerger()
+    mergePDF = PDF()
 
     validFormats = dict()
     commonArgs = (fileMap, mergePDF)
@@ -53,12 +52,12 @@ def processInstructions(instructions: str, fileMap: dict[str, pdf.PdfReader], sa
         if result != True:
             return f"Instruction \"{instruction}\": {result}"
 
-    if len(mergePDF.pages) == 0:
+    if len(mergePDF) == 0:
         return "No instructions given; PDF would have been empty"    
     saveMergedPDF(savepath, mergePDF)
     return None
 
-def registerNewFiles(newFiles: tuple[str], fileMap: dict[str, pdf.PdfReader]) -> (str, bool | str):
+def registerNewFiles(newFiles: tuple[str], fileMap: dict[str, "PDF"]) -> (str, bool | str):
     text = ""
     for i in range(len(newFiles)):
         if len(fileMap) == 26:
@@ -66,10 +65,23 @@ def registerNewFiles(newFiles: tuple[str], fileMap: dict[str, pdf.PdfReader]) ->
         char = chr(ord('a') + len(fileMap))
         filename = os.path.basename(newFiles[i])
         try:
-            reader = pdf.PdfReader(newFiles[i])
+            reader = PDF(newFiles[i])
         except Exception as e:
             return text, f"\"{newFiles[i]}\" could not be read as a PDF"
         fileMap[char] = reader
         text += f"{char}:\t{filename}\n"
     return text, False
 
+
+class PDF:
+    def __init__(this, path=None) -> None:
+        this.obj = fitz.open(path)
+
+    def __len__(this) -> int:
+        return this.obj.page_count
+
+    def appendPages(this, source: "PDF", start: int, end: int) -> None:
+        this.obj.insert_pdf(source.obj, from_page=start-1, to_page=end-1)
+
+    def writeFile(this, path: str) -> None:
+        this.obj.save(path)
